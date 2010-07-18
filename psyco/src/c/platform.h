72d5e9181a4dec2609745b21bc1131fa9ca37371
tom <tom@54af9aa6-5a1e-0410-a967-addfb4fd9e76>
2010-07-18 17:18:25 +0000

    git-svn-id: http://dev.nodebox.net/svn/nodebox-gl/trunk@1065 54af9aa6-5a1e-0410-a967-addfb4fd9e76

--- psyco/src/c/platform.h
+++
@@ -1,9 +0,0 @@
-/*
- * OS-specific utilities.
- */
-
-/* allocate 'basicsize' bytes, or possibly a multiple of it if allocating
-   large blocks of memory is better.  The allocated address is stored
-   in '*result'.  The total allocated size is returned, or 0 to try some
-   other allocation method. */
-extern long psyco_allocate_executable_buffer(long basicsize, char **result);
