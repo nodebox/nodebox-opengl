d15fdf9776933fc5ebe5843ee56b4e974662987f
tom <tom@54af9aa6-5a1e-0410-a967-addfb4fd9e76>
2010-08-06 19:26:02 +0000

    git-svn-id: http://dev.nodebox.net/svn/nodebox-gl/trunk@1076 54af9aa6-5a1e-0410-a967-addfb4fd9e76

--- examples/test.py
+++ examples/test.py
@@ -3,22 +3,6 @@ import os, sys; sys.path.append(os.path.join(".."))
 
 from nodebox.graphics import *
 
-
-from nodebox.gui import *
-
-panel = Panel("Example", width=200, height=200)
-panel.append(
-    Rows(controls=[
-      Field(value="hello world", hint="text"),
-      ("size",    Slider(default=1.0, min=0.0, max=2.0, steps=100),
-      ("opacity", Slider(default=1.0, min=0.0, max=1.0, steps=100),
-      ("visible", Flag(default=True)),
-      Button("Reset", action=lambda button: None)]))
-panel.pack()
-canvas.append(panel)
-canvas.run()
-
-
 def draw(canvas):
     background(1)
     translate(250, 250)
