2dde5a7eb4a0f379e8d2476487011bdacba7e165
Tom De Smedt <tom@organisms.be>
2011-04-24 18:32:37 +0200

    Live coding example in examples/12-experimental/

---
+++ examples/12-experimental/04-live2.py
@@ -0,0 +1,12 @@
+# See 04-live1.py.
+# The commands below are read and executed in a canvas there.
+# If you edit and save this file, changes are automatically reflected.
+
+#canvas.clear()
+for i in range(10):
+    x = random(canvas.width)
+    y = random(canvas.height)
+    r = random(20) + 50
+    fill(random(), 0.0, random(0.25), random(0.25))
+    stroke(0, 0.2)
+    ellipse(x, y, r, r)
