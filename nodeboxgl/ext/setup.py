fc610faafd2ecbe02bddd5fe454860d25c2eaf37
Karsten Wolf <karstenwo@web.de>
2017-05-31 16:00:24 +0200

    Renamed 'extensions' to 'Nodebox for OpenGL c-extensions'

--- nodeboxgl/ext/setup.py
+++ nodeboxgl/ext/setup.py
@@ -5,7 +5,7 @@ geometry = Extension("nglgeometry", sources=["geometry.c"])
 noise    = Extension("nglnoise",    sources=["noise.c"])
 
 setup(
-         name = "extensions",
+         name = "Nodebox for OpenGL c-extensions",
       version = "1.0",
        author = "Tom De Smedt, Frederik De Bleser",
   description = "Fast C Bezier, geometry and noise math.",
